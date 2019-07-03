73bbe749c21b807836bac5809dfeb67d8a8314a3
AustinBH <harlowaustin2@gmail.com>
2019-07-03 15:27:22 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+function map(array, func) {
+  let newArray = []
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(func(array[i]))
+  }
+  return newArray
+}
+
+function reduce(array, func, startingPoint = 0) {
+  let result = startingPoint
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === 'number')
+      result += array[i]
+    else if (array[i] === false){
+      result = false
+    }
+    else if (array[i] === true){
+      result = true
+    }
+  }
+  return result
+}
