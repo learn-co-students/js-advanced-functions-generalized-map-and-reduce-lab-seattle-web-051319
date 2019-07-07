39b0dbd9ae12afe66dbfe707cb9f98f1e5fa5a01
jenshaw86 <bloo.j.shaw@gmail.com>
2019-07-07 09:13:57 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your functions here
+const map = (arr, func) => {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        let newVal = func(arr[i]);
+        newArr.push(newVal);
+    }
+    return newArr;
+}
+
+const reduce = (arr, func, start) => {
+    let currVal = !!start ? start : arr[0];
+    let i = !!start ? 0 : 1;
+    for (; i < arr.length; i++) {
+        currVal = func(arr[i], currVal);
+    } 
+    return currVal;
+}
